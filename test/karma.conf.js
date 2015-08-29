daef2a4a531492c46b933c5c952208a6907a54c0
Federico Yankelevich <yankedev@exteso.com>
2015-08-29 23:55:24 +0200

    add numeral.js (as required by Celestino) to calculate and format numbers

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -36,6 +36,7 @@ module.exports = function(config) {
       'bower_components/angularfire/dist/angularfire.js',
       'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
       'bower_components/lodash/lodash.js',
+      'bower_components/numeral/numeral.js',
       'bower_components/mockfirebase/browser/mockfirebase.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
