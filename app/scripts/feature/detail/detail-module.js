df3dcc69606d4db067aad9317ca5fe3aef93c633
Celestino Bellone <celestino@exteso.com>
2015-08-29 20:12:52 +0200

    insert new location

--- app/scripts/feature/detail/detail-module.js
+++ app/scripts/feature/detail/detail-module.js
@@ -1,17 +1,22 @@
 (function() {
     "use strict";
-    angular.module('accessopolis.locationDetail', [])
+    angular.module('accessopolis.locationDetail', ['accessopolis.navigation'])
         .service('LocationDetailService', LocationDetailService)
         .controller('LocationDetailController', LocationDetailController)
+        .controller('NewLocationController', NewLocationController)
         .config(['$routeProvider', function($routeProvider) {
             $routeProvider.when('/locations/:id', {
-                    templateUrl: 'scripts/feature/detail/detail.html',
-                    controller: 'LocationDetailController',
-                    controllerAs: 'ctrl'
-                });
+                templateUrl: 'scripts/feature/detail/detail.html',
+                controller: 'LocationDetailController',
+                controllerAs: 'ctrl'
+            }).when('/new-location', {
+                templateUrl: 'scripts/feature/detail/new.html',
+                controller: 'NewLocationController',
+                controllerAs: 'ctrl'
+            });
         }]);
 
-    function LocationDetailService($q, $firebaseObject, Ref) {
+    function LocationDetailService($q, $firebaseObject, Ref, $firebaseArray) {
         this.find = function(id) {
             return $q(function(resolve, reject) {
                 var obj = $firebaseObject(Ref.child('locations').orderByKey().equalTo(id));
@@ -20,11 +25,14 @@
                 });
             });
         };
+
+        this.create = function(location) {
+            var mock = {lat: 45.833376, long: 9.030515};
+            return $firebaseArray(Ref.child('locations')).$add(angular.extend(mock, location));
+        }
     }
 
-
-
-    LocationDetailService.prototype.$inject = ['$q', '$firebaseObject', 'Ref'];
+    LocationDetailService.prototype.$inject = ['$q', '$firebaseObject', 'Ref', '$firebaseArray'];
 
     function LocationDetailController(LocationDetailService, $routeParams, $location) {
 
@@ -38,5 +46,32 @@
         };
     }
 
-    LocationDetailController.prototype.$inject = ['LocationSearchService', '$routeParams', '$location'];
+    LocationDetailController.prototype.$inject = ['LocationDetailService', '$routeParams', '$location'];
+
+    function NewLocationController(NavigationService, LocationDetailService, $location) {
+        var self = this;
+        this.location = {};
+        //this.stars = _.range(0,6);
+
+        this.save = function(frm) {
+            if(!frm.$valid) {
+                return;
+            }
+            LocationDetailService.create(self.location).then(function(data) {
+                $location.path('/locations/'+data.name());
+            }, function(err) {
+                alert(err);
+            });
+        };
+
+        NavigationService.loadNavigationElements().then(function(result) {
+            result.$loaded(function(data) {
+                self.subtypes = _.chain(data).map('subcategory').flatten().uniq().value();
+            });
+        });
+
+    }
+
+    NewLocationController.prototype.$inject = ['NavigationService', 'LocationDetailService', '$location'];
+
 })();
