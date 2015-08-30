9e74c60bfc9d1b4db78d192010b913743160f5f4
Celestino Bellone <celestino@exteso.com>
2015-08-30 12:58:15 +0200

    show map on detail

--- app/scripts/feature/detail/detail-module.js
+++ app/scripts/feature/detail/detail-module.js
@@ -39,6 +39,23 @@
             }
 
         }])
+        .directive('imageMap', function() {
+            return {
+                restrict: 'A',
+                scope: true,
+                controllerAs: 'imageCtrl',
+                controller: function() {
+                    var self = this;
+                    this.showImage = function() {
+                        return angular.isDefined(self.location) && angular.isDefined(self.location.lat) && angular.isDefined(self.location.long);
+                    }
+                },
+                bindToController: {
+                    location: '=imageMap'
+                },
+                template: '<a href="https://google.com/maps?z=12&t=m&q=loc:{{imageCtrl.location.lat}}+{{imageCtrl.location.long}}" title="google maps" data-ng-if="imageCtrl.showImage()" target="_blank"><img class="img-responsive" data-ng-src="https://maps.googleapis.com/maps/api/staticmap?center={{imageCtrl.location.lat}},{{imageCtrl.location.long}}&zoom=13&size=200x200&maptype=roadmap&markers=color:red%7Clabel:C%7C{{imageCtrl.location.lat}},{{imageCtrl.location.long}}" /></a>'
+            }
+        })
         .config(['$routeProvider', function($routeProvider) {
             $routeProvider.when('/locations/:id', {
                 templateUrl: 'scripts/feature/detail/detail.html',
