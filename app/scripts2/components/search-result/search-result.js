b843b2ef0cb4a72ff22066760d588cf380b14392
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-11-30 20:01:50 +0100

    #48 basic display of results

--- app/scripts2/components/search-result/search-result.js
+++ app/scripts2/components/search-result/search-result.js
@@ -4,7 +4,23 @@
     bindings: {
       result: '='
     },
-    template: ['<div>{{apSearchResult.result}}</div>'].join('')
+    template: ['<div><div>',
+                  '<div class="ap-search-result-icon-container">', iconTemplate(),'</div>',
+                  '<div class="ap-search-result-description">{{apSearchResult.result}}</div>',
+               '</div></div>'].join('')
   })
+  
+  
+  
+  
+  function iconTemplate() {
+  
+    return [ '<span class="fa fa-thumb-tack" ng-class="{\'fa-coffee\': apSearchResult.result.type === \'bar\',',
+                         '\'fa-cutlery\': apSearchResult.result.type == \'restaurant\',',
+                         '\'fa-bed\': apSearchResult.result.type === \'hotels\',',
+                         '\'fa-beer\': apSearchResult.result.type === \'pub\',',
+                         '\'fa-train\': apSearchResult.result.type === \'train-stations\'}">',
+            '</span>'].join('');
+  }
   
 })();
