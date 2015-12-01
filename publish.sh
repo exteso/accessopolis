2ce76dbdee98ee1b2e0f5f0e63938d275e4d11b6
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-12-01 23:09:02 +0100

    #48 update publish to v2 page

--- publish.sh
+++ publish.sh
@@ -7,6 +7,7 @@ rm -Rf .tmp/gh-pages/*
 cp -Rp dist/* .tmp/gh-pages
 cd .tmp/gh-pages
 sed -i '' 's/accessopolis-dev.firebaseio.com/accessopolis.firebaseio.com/g' ./app/scripts/angularfire/config.js
+sed -i '' 's/accessopolis-dev.firebaseio.com/accessopolis.firebaseio.com/g' ./app/scripts2/app.js
 git add .
 git commit -m "new website version"
 git push origin
