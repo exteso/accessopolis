05889642ed5a2e480acce9b08f2d4c4350b3e62a
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-12-01 21:40:01 +0100

    #48 display comments

--- app/scripts2/components/location/location.js
+++ app/scripts2/components/location/location.js
@@ -1,12 +1,18 @@
 (function() {
 
   angular.module('accessopolisApp').component('apLocation', {
-    template:topTemplate(),
+  
+    template: topTemplate() + bodyTemplate(),
+    
     controller: ['$routeParams', '$location', 'LocationService', function($routeParams, $location, LocationService) {
       var vm = this;
       
       LocationService.find($routeParams.identifier).then(function(location) {
         vm.location = location;
+      });
+      
+      LocationService.getComments($routeParams.identifier).then(function(comments) {
+        vm.comments = comments;
       });
       
       function backToHome() {
@@ -19,7 +25,6 @@
   })
 
   function topTemplate() {
-
     return [
     '<div class="accessopolis-location-detail col-xs-12" style="background: #000 !important;">',
         '<div class="container accessopolis-location-detail-header-wrap">',
@@ -37,6 +42,32 @@
       '</div>',
     '</div>'
     ].join('');
+  }
+  
+  function bodyTemplate() {
+    return ['<div class="col-xs-12">',
+              '<div class="container accessopolis-location-detail-content">',
+                '<div class="col-md-12 col-sm-12 col-xs-12 accessopolis-bg-white" style="padding-bottom: 50px; margin-bottom: 20px;  margin-top: 10px;">', //comments
+                   '<h1>Commenti:</h1>',
+                   '<ul class="list-group col-md-12 col-sm-12 col-xs-12" style="list-style-type: none">',
+                    commentTemplate(),
+                   '</ul>',
+                '</div>',
+              '</div>',
+            '</div>'].join('');
+  }
+  
+  
+  function commentTemplate() {
+    return ['<li class="list-group-item" ng-repeat="comment in apLocation.comments track by comment.$id" style="float: left; width: 100%;">',
+              '<div style="width: 50px; height: 50px; float: left; ">',
+                '<div style="float: left; width: 50px; height: 50px; background: #ccc; color:#fff; text-align: center; border-radius: 50%;  line-height: 50px;">',
+                  '<span class="fa fa-user"></span>',
+                '</div>',
+              '</div>',
+              '<div style="float: left; height: 50px; line-height: 50px; margin-left: 10px; " ng-bind="::comment.text"></div>',
+            '</li>',
+            '<li ng-if="apLocation.comments.length == 0">Nessun commento!</li>'].join('');
   }
 
 
