004760d4e0fe8da3499cbe7eee99215134f617e7
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-12-07 15:57:41 +0100

    Update scripts.0999461e.js
    fix firebaseurl...

--- scripts/scripts.0999461e.js
+++ scripts/scripts.0999461e.js
@@ -209,7 +209,7 @@ angular.module('accessopolisApp')
   }]);
 
 angular.module('firebase.config', [])
-  .constant('FBURL', 'https://accessopolis-dev.firebaseio.com')
+  .constant('FBURL', 'https://accessopolis.firebaseio.com')
   .constant('SIMPLE_LOGIN_PROVIDERS', ['google'])
 
   .constant('loginRedirectPath', '/login');
