68012cb25252eb0cd44f7c9ba6adb3c76bb05ac0
Federico Yankelevich <yankedev@exteso.com>
2016-01-11 00:37:15 +0100

    user service is now stateful

--- app/scripts2/services/user-service.js
+++ app/scripts2/services/user-service.js
@@ -1,22 +1,45 @@
 (function() {
 
-angular.module('accessopolisApp').service('UserService', ['$q', '$firebaseObject', 'Ref', '$firebaseArray', UserService]);
+angular.module('accessopolisApp').service('UserService', ['Auth', '$q', '$firebaseObject', 'Ref', '$firebaseArray', UserService]);
 
+function UserService(Auth, $q, $firebaseObject, Ref, $firebaseArray) {
 
-function UserService($q, $firebaseObject, Ref, $firebaseArray) {
+    var user;
+    var profile;
 
-  function find(id) {
-        return $q(function(resolve, reject) {
-            $firebaseObject(Ref.child('users/'+id)).$loaded(function(val) {
+    Auth.$onAuth(function (authData) {
+        if (authData) {
+            user = Auth.$getAuth();
+            find(user.uid).then(function(profileFromDB){
+                profile = profileFromDB;
+            })
+        } else {
+            user = undefined;
+            profile = undefined;
+        }
+    });
+
+    function getUser() {
+        return user;
+    }
+
+    function getProfile() {
+        return profile;
+    }
+
+    function find(id) {
+        return $q(function (resolve, reject) {
+            $firebaseObject(Ref.child('users/' + id)).$loaded(function (val) {
                 resolve(val);
             });
         });
     };
-    
+
     //
     this.find = find;
+    this.getUser = getUser;
+    this.getProfile = getProfile;
 }
-
 })();
 
 
