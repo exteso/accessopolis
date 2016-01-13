4d611046906427fa90468832bda264fb18d78552
Federico Yankelevich <yankedev@exteso.com>
2016-01-13 16:58:29 +0100

    add edit location

--- app/scripts2/components/new-location/new-location.js
+++ app/scripts2/components/new-location/new-location.js
@@ -3,12 +3,21 @@
   angular.module('accessopolisApp').component('apNewLocation', {
 
     templateUrl: 'scripts2/components/new-location/new-location.html',
+    bindings: {
+      identifier:'=',
+    },
     controller: ['$location', 'LocationService', 'Auth', function($location, LocationService, Auth) {
       var vm = this;
 
       Auth.$onAuth(function(authData) {
         vm.isAuth = !!authData;
       });
+
+      if (this.identifier) {
+          LocationService.find(this.identifier).then(function (location) {
+              vm.location = location;
+          });
+      }
 
       LocationService.loadNavigationElements().then(function(subtypes){
         vm.subtypes = subtypes;
@@ -18,12 +27,12 @@
         if(!frm.$valid) {
             return;
         }
-        
+
         LocationService
           .create(vm.location)
           .then(function (data) {$location.path('/location/' + data.key());}, function (err) {alert(err);})
       };
-      
+
       function onAddressSelected(address) {
         vm.location = vm.location || {};
         vm.location.address = address.formatted_address;
@@ -31,7 +40,7 @@
         vm.location.long = address.geometry.location.lng();
       };
 
-      
+
       function backToHome() {
         $location.path('/');
       }
@@ -39,7 +48,7 @@
       this.backToHome = backToHome;
       this.save = save;
       this.onAddressSelected = onAddressSelected;
-      
+
     }]
   })
 
